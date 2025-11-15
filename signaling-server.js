b95e61a9a3be005e724b2d4d27fa48107c4f11f8
Vivek Singh <71491162+vviveksingh33@users.noreply.github.com>
2025-11-15 16:07:04 +0530

    Refactor signaling server to use Socket.IO

--- signaling-server.js
+++ signaling-server.js
@@ -1,45 +1,48 @@
-const WebSocket = require("ws");
-const PORT = process.env.PORT || 3000;
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
-const wss = new WebSocket.Server({ port: PORT });
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
 
-console.log("Signaling server running on port:", PORT);
+const rooms = new Map();
+function genCode(len = 6) { return Math.random().toString(36).slice(2, 2 + len).toUpperCase(); }
 
-let rooms = {}; // roomId: [clients]
+io.on('connection', socket => {
+  console.log('socket connected', socket.id);
 
-wss.on("connection", (ws) => {
-    ws.on("message", (msg) => {
-        let data;
-        try {
-            data = JSON.parse(msg);
-        } catch (err) {
-            return;
-        }
+  socket.on('create', (opts = {}, cb = () => {}) => {
+    const code = genCode();
+    rooms.set(code, { host: socket.id, peers: new Set() });
+    socket.join(code);
+    console.log('room created', code);
+    cb({ ok: true, code });
+  });
 
-        const { type, roomId } = data;
+  socket.on('join', (code, cb = () => {}) => {
+    const room = rooms.get(code);
+    if (!room) return cb({ ok: false, msg: 'Room not found' });
+    room.peers.add(socket.id);
+    socket.join(code);
+    io.to(room.host).emit('peer-joined', { peer: socket.id, code });
+    console.log('peer joined', socket.id, 'in', code);
+    cb({ ok: true });
+  });
 
-        if (type === "join") {
-            if (!rooms[roomId]) rooms[roomId] = [];
-            rooms[roomId].push(ws);
-            ws.roomId = roomId;
+  socket.on('signal', ({ code, to, data }) => {
+    if (to) io.to(to).emit('signal', { from: socket.id, data });
+    else if (code) socket.to(code).emit('signal', { from: socket.id, data });
+  });
 
-            console.log("Client joined room:", roomId);
-        }
-
-        // forward message to all peers in the room
-        if (rooms[roomId]) {
-            rooms[roomId].forEach((client) => {
-                if (client !== ws && client.readyState === WebSocket.OPEN) {
-                    client.send(msg);
-                }
-            });
-        }
-    });
-
-    ws.on("close", () => {
-        const roomId = ws.roomId;
-        if (roomId && rooms[roomId]) {
-            rooms[roomId] = rooms[roomId].filter((c) => c !== ws);
-        }
-    });
+  socket.on('disconnect', () => {
+    for (const [code, r] of rooms.entries()) {
+      if (r.host === socket.id) { io.to(code).emit('host-left'); rooms.delete(code); }
+      else r.peers.delete(socket.id);
+    }
+  });
 });
+
+app.get('/', (req, res) => res.send('Socket.IO signaling server running'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log('listening', PORT));
